eb729e922fc7ab4be166a22743b3636b0cf990dc
drewry@google.com <drewry@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-10-16 21:39:47 +0000

    Remove the verifying file system since we'll probably go device-mapper now.
    Review URL: http://chromereview.prom.corp.google.com/1176052
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@60 06c00378-0e64-4dae-be16-12b19f9950a1

--- src/platform/verity/fuse_bridge.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef __CHROMEOS_VERITY_FUSE_BRIDGE_H
-#define __CHROMEOS_VERITY_FUSE_BRIDGE_H
-extern const struct fuse_operations *chromeos_verity_operations;
-#endif  // __CHROMEOS_VERITY_FUSE_BRIDGE_H
-
