7cb8ccce265e90883bd8de0b6f67bb3012d0eb1e
adlr@google.com <adlr@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-11-09 18:40:26 +0000

    AU: Use upstart; check every 30 min
    Review URL: http://chromereview.prom.corp.google.com/1187004
    
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@186 06c00378-0e64-4dae-be16-12b19f9950a1

--- src/platform/memento_softwareupdate/software_update.sh
+++ src/platform/memento_softwareupdate/software_update.sh
@@ -7,10 +7,26 @@
 # Called on startup as part of the init scripts. This calls forks off
 # a task which will wait some time and then check for an update.
 
-if [ "$1" = "start" ]
-then
-  {
-    sleep 120  # seconds
-    /opt/google/memento_updater/memento_updater.sh
-  } &
+# Exit immediately if it's a dev machine
+DEVKIT_URL=$(grep ^CHROMEOS_DEVSERVER /etc/lsb-release | cut -d = -f 2-) 
+if [ -n "$DEVKIT_URL" ]; then
+  exit 0
 fi
+
+if [ -z "$1" ]; then
+  SLEEP_TIME=1800  # seconds; 30 min
+else
+  SLEEP_TIME="$1"
+fi
+
+# Boot after 2 min, then try every 30 min
+sleep 120  # seconds
+# Once at startup, if log file is too big, move it aside
+LOG_FILE=/var/log/softwareupdate.log
+if [ $(stat -c%s "$LOG_FILE") -gt "5242880" ]; then  # 5MB
+  mv "$LOG_FILE" "$LOG_FILE".0
+fi
+while [ true ]; do
+  /opt/google/memento_updater/memento_updater.sh
+  sleep "$SLEEP_TIME"
+done
