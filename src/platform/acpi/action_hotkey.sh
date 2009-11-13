76af24ff4086e87947bb0e7510991fdee8624f7f
tedbo@google.com <tedbo@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-11-13 00:04:19 +0000

    Add acpi pacakge for our acpi scripts.
    This removes the acpi stuff from customize_rootfs and should make it easier
    to do more acpi stuff in the future as needed. We still need to figure out
    how to handle acpi across different devices.
    
    Review URL: http://chromereview.prom.corp.google.com/1186038
    
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@220 06c00378-0e64-4dae-be16-12b19f9950a1

---
+++ src/platform/acpi/action_hotkey.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# TODO: This is specific to eeepc. Figure out a way to generalize and
+# add support for additional hotkeys.
+
+case "$3" in
+  00000013)  # Toggle sound
+        amixer set LineOut toggle
+        amixer set iSpeaker toggle
+        ;;
+  00000014)  # Decrease volume
+        amixer set LineOut 5%-
+        ;;
+  00000015)  # Increase volume
+        amixer set LineOut 5%+
+        ;;
+esac
---
+++ src/platform/acpi/action_lid.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# On lid close:
+# - lock the screen
+export HOME=/home/chronos
+/usr/bin/xscreensaver-command -l
+
+# - suspend the cryptohome device
+#CRYPTOHOME=/dev/mapper/cryptohome
+#/usr/bin/test -b $CRYPTOHOME && /sbin/dmsetup suspend $CRYPTOHOME
+
+# - stop wireless if UP
+wlan0isup=`/sbin/ifconfig wlan0 2>/dev/null | /bin/grep UP`
+test "$wlan0isup" && /sbin/ifconfig wlan0 down
+
+# - suspend to ram
+echo -n mem > /sys/power/state
+
+# On lid open:
+# - restore wireless state
+test "$wlan0isup" && /sbin/ifconfig wlan0 up
+
+# - resume cryptohome device
+#/usr/bin/test -b $CRYPTOHOME && /sbin/dmsetup resume $CRYPTOHOME
