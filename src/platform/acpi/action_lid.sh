cde805f71ccc6d918ca049b347245ab773e9e911
sosa@google.com <sosa@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-11-19 02:06:17 +0000

    Fix to xscreensaver so that you don't have to press a key twice to show lock screen
    Review URL: http://chromereview.prom.corp.google.com/1188086
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@289 06c00378-0e64-4dae-be16-12b19f9950a1

--- src/platform/acpi/action_lid.sh
+++ src/platform/acpi/action_lid.sh
@@ -24,5 +24,9 @@ echo -n mem > /sys/power/state
 # - restore wireless state
 test "$wlan0isup" && /sbin/ifconfig wlan0 up
 
+# On lid open:
+# - has it such that you don't have to press a key to display lock screen
+/usr/bin/xscreensaver-command -deactivate
+
 # - resume cryptohome device
 #/usr/bin/test -b $CRYPTOHOME && /sbin/dmsetup resume $CRYPTOHOME
