ba3bdd89fa17ebd799cef8ad87ebd8279d98bfa4
sleffler@google.com <sleffler@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-11-24 21:09:48 +0000

    remove suspend/resume hack now that mac80211 dtrt
    Review URL: http://codereview.chromium.org/436030
    
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@314 06c00378-0e64-4dae-be16-12b19f9950a1

--- src/platform/acpi/action_lid.sh
+++ src/platform/acpi/action_lid.sh
@@ -13,16 +13,8 @@ export HOME=/home/chronos
 #CRYPTOHOME=/dev/mapper/cryptohome
 #/usr/bin/test -b $CRYPTOHOME && /sbin/dmsetup suspend $CRYPTOHOME
 
-# - stop wireless if UP
-wlan0isup=`/sbin/ifconfig wlan0 2>/dev/null | /bin/grep UP`
-test "$wlan0isup" && /sbin/ifconfig wlan0 down
-
 # - suspend to ram
 echo -n mem > /sys/power/state
-
-# On lid open:
-# - restore wireless state
-test "$wlan0isup" && /sbin/ifconfig wlan0 up
 
 # On lid open:
 # - has it such that you don't have to press a key to display lock screen
