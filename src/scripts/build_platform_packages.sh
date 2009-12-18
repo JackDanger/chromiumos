JackDanger/chromiumos

commit da86fbcbbdff4fdb44071f241525af23aafd36cc

Daemonize (fork()) and install metrics_daemon in the image for execution. With this change ChromeOS begins to send metrics to the UMA server.
Review URL: http://codereview.chromium.org/500117

src/scripts/build_platform_packages.sh
@@ -30,7 +30,7 @@ PLATFORM_DIRS="acpi assets fake_hal init installer login_manager \
                memento_softwareupdate pam_google window_manager \
                cros chrome screenlocker cryptohome \
                monitor_reconfig microbenchmark minijail metrics_collection \
-               theme"
+               theme metrics_daemon
 
 THIRD_PARTY_DIR="$SRC_ROOT/third_party"
 THIRD_PARTY_PACKAGES="connman e2fsprogs/files \

src/platform/init/debian/rules
@@ -47,6 +47,7 @@ install:
 	   $(CURDIR)/udev.conf               \
 	   $(CURDIR)/ui.conf                 \
 	   $(CURDIR)/wpasupplicant.conf      \
+	   $(CURDIR)/metrics_daemon.conf     \
 	   $(CURDIR)/debian/chromeos-init/etc/init.chromeos/
 
 # Build architecture-independent files here.

src/platform/init/metrics_daemon.conf
@@ -1,0 +1,12 @@
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# metrics_daemon
+
+# We'd like to catch the early network stats.
+start on started connman
+
+respawn
+expect fork
+exec /usr/bin/metrics_daemon

src/platform/metrics_daemon/Makefile
@@ -12,7 +12,7 @@ CCONFIG = $(shell pkg-config --cflags dbus-1 glib-2.0 dbus-glib-1)
 LDCONFIG = $(shell pkg-config --libs dbus-1 glib-2.0 gthread-2.0 dbus-glib-1)
 
 CFLAGS = $(COPT) -fno-exceptions -Wall -Werror $(INCLUDES) $(CCONFIG)
-LDFLAGS = -L../../third_party/chrome $(LDCONFIG) -lrt -lbase -lpthread
+LDFLAGS = -L../../third_party/chrome $(LDCONFIG) -lrt -lbase -lpthread -lgflags
 
 COMMONSRC = metrics_daemon.cc
 COMMONCSRC = marshal_void__string_boxed.c
@@ -58,7 +58,8 @@ marshal_void__string_boxed.o: marshal_void__string_boxed.h
 .PRECIOUS: marshal_void__string_boxed.c  # keep around for debugging
 
 install:
-	install $DAEMON $(DESTDIR)/usr/bin
+	mkdir -p $(DESTDIR)/usr/bin
+	install $(DAEMON) $(DESTDIR)/usr/bin
 
 clean:
 	rm -f *.o marshal_void__string_boxed.[ch] \

src/platform/metrics_daemon/debian/control
@@ -10,7 +10,7 @@ Build-Depends: debhelper (>=7)
 Standards-Version: 3.8.0
 
 Package: chromeos-metrics-daemon
-Architecture: all
+Architecture: i386
 Depends: ${shlibs:Depends}, ${misc:Depends}
 Description: Metrics daemon for Chrome OS.
  The metrics daemon collects events and sends them to UMA via Chrome.

src/platform/metrics_daemon/debian/rules
@@ -33,6 +33,7 @@ install: build
 	dh_testroot
 	dh_clean -k
 	dh_installdirs
+	mkdir -p $(CURDIR)/debian/chromeos-metrics-daemon
 	$(MAKE) DESTDIR=$(CURDIR)/debian/chromeos-metrics-daemon install
 
 binary-indep:

src/platform/metrics_daemon/main.cc
@@ -3,9 +3,14 @@
 // found in the LICENSE file.
 
 
+#include <gflags/gflags.h>
+
 #include "platform/metrics_daemon/metrics_daemon.h"
+
+DEFINE_bool(daemon, true, "run as daemon (use -nodaemon for debugging)");
 
 int main(int argc, char** argv) {
   MetricsDaemon::MetricsDaemon d;
-  d.Run(false);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  d.Run(FLAGS_daemon, false);
 }

commit 076364244fdb21984dd2cc4201f6d60af2e0df9d

Add missing quote to build_platform_packages.sh.
Broken by da86fbcb.

TBR=semenzato

Review URL: http://codereview.chromium.org/504055

src/scripts/build_platform_packages.sh
@@ -30,7 +30,7 @@ PLATFORM_DIRS="acpi assets fake_hal init installer login_manager \
                memento_softwareupdate pam_google window_manager \
                cros chrome screenlocker cryptohome \
                monitor_reconfig microbenchmark minijail metrics_collection \
-               theme metrics_daemon
+               theme metrics_daemon"
 
 THIRD_PARTY_DIR="$SRC_ROOT/third_party"
 THIRD_PARTY_PACKAGES="connman e2fsprogs/files \