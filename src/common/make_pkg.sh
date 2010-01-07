5f92aafbbdb381d247a2c42148f35a707e76748b
Colin Watson <cjwatson@canonical.com>
2010-01-07 00:09:56 +0000

    Install libchrome-dev and libchromeos-dev packages
    Add make_pkg.sh for src/third_party/chrome/ (a.k.a. libchrome-dev) and
    src/common/ (a.k.a. libchromeos-dev) and (ab)use it to install those
    packages once built.
    
    Installing the packages from make_pkg.sh isn't very clean. However, this
    gives us a migration path to future use of chromeos-build, which will
    need these headers and libraries to be built as packages in order to
    support cross-building cleanly.
    
    Review URL: http://codereview.chromium.org/523109

---
+++ src/common/make_pkg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Builds the .deb package.
+
+# Load common constants.  This should be the first executable line.
+# The path to common.sh should be relative to your script's location.
+COMMON_SH="$(dirname "$0")/../scripts/common.sh"
+. "$COMMON_SH"
+
+PKG_BASE="libchromeos"
+
+# All packages are built in the chroot
+assert_inside_chroot
+
+# Command line options
+DEFINE_string build_root "$DEFAULT_BUILD_ROOT" "Root of build output"
+
+# Parse command line and update positional args
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+# Die on any errors
+set -e
+
+# Make output dir
+OUT_DIR="$FLAGS_build_root/x86/local_packages"
+mkdir -p "${OUT_DIR}"
+
+# Remove previous package from output dir
+rm -f "${OUT_DIR}/${PKG_BASE}"-dev_*.deb
+
+# Rebuild the package
+pushd "$TOP_SCRIPT_DIR"
+rm -f ../${PKG_BASE}-dev_*.deb
+dpkg-buildpackage -b -tc -us -uc -j$NUM_JOBS
+mv ../${PKG_BASE}-dev_*.deb "$OUT_DIR"
+rm ../${PKG_BASE}_*.changes
+popd
+
+# Install packages that are necessary for building later packages.
+sudo -E dpkg -i "${OUT_DIR}/${PKG_BASE}"-dev_*.deb
