ea1a7b84df80201d020b57b7c27585cbf0befd82
Colin Watson <cjwatson@canonical.com>
2010-01-08 18:34:49 +0000

    synaptics: Add Debian packaging files
    Add Debian packaging, suitable for native- and cross-building.
    
    Review URL: http://codereview.chromium.org/521077

--- src/third_party/synaptics/make_pkg.sh
+++ src/third_party/synaptics/make_pkg.sh
@@ -11,4 +11,35 @@
 COMMON_SH="$(dirname "$0")/../../scripts/common.sh"
 . "$COMMON_SH"
 
-scons -j$NUM_JOBS
+PKG_BASE="chromeos-synaptics"
+
+# All packages are built in the chroot
+assert_inside_chroot
+
+# Command line options
+DEFINE_string build_root "$DEFAULT_BUILD_ROOT" "Root of build output"
+
+# Parse command line and update positional args
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+# Die on any errors
+set -e
+
+# Make output dir
+OUT_DIR="$FLAGS_build_root/x86/local_packages"
+mkdir -p "${OUT_DIR}"
+
+# Remove previous package from output dir
+rm -f "${OUT_DIR}/lib${PKG_BASE}"-dev_*.deb
+
+# Rebuild the package
+pushd "$TOP_SCRIPT_DIR"
+rm -f ../lib${PKG_BASE}-dev_*.deb
+dpkg-buildpackage -b -us -uc -j$NUM_JOBS
+mv ../lib${PKG_BASE}-dev_*.deb "$OUT_DIR"
+rm ../${PKG_BASE}_*.changes
+popd
+
+# Install packages that are necessary for building later packages.
+sudo -E dpkg -i "${OUT_DIR}/lib${PKG_BASE}"-dev_*.deb
