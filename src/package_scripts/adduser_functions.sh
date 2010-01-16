822e7d2f8688aca3b25e09cfb965f387f63875e5
Andrew de los Reyes <opensource@google.com>
2010-01-15 21:11:43 -0800

    Arm build: dbus postinst script
    Also, script to handle build hard-coded uids and gids.
    
    Review URL: http://codereview.chromium.org/543103

---
+++ src/package_scripts/adduser_functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Each package should list its hard-coded UIDs/GIDs here. We are starting at
+# 200 and going up
+DBUS_UID=200
+DBUS_GID=200
+
+# Call with these params:
+# username (e.g. "messagebus")
+# UID (e.g. 200)
+# GID (e.g. 200)
+# full name (e.g. "")
+# home dir (e.g. "/home/foo" or "/var/run/dbus")
+# shell (e.g. "/bin/sh" or "/bin/false")
+add_user() {
+  echo "${1}:x:${2}:${3}:${4}:${5}:${6}" | \
+    dd of="${ROOT}/etc/passwd" conv=notrunc oflag=append
+}
+
+# Call with these params:
+# groupname (e.g. "messagebus")
+# GID (e.g. 200)
+add_group() {
+  echo "${1}:x:${2}:" | dd of="${ROOT}/etc/group" conv=notrunc oflag=append
+}
