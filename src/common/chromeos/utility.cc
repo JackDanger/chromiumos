225d690d64ca1c10e79a23b50de037de216be5e3
Chris Masone <cmasone@chromium.org>
2010-02-22 14:33:20 -0800

    add AsciiEncode/Decode to libcromeos
    Review URL: http://codereview.chromium.org/650174

--- src/common/chromeos/utility.cc
+++ src/common/chromeos/utility.cc
@@ -5,8 +5,53 @@
 #include <chromeos/utility.h>
 
 #include <cstring>
+#include <string>
+#include <vector>
+
+#include "chromeos/obsolete_logging.h"
 
 namespace chromeos {
+
+using std::string;
+
+char DecodeChar(char in) {
+  in = tolower(in);
+  if ((in <= '9') && (in >= '0')) {
+    return in - '0';
+  } else {
+    CHECK_GE(in, 'a');
+    CHECK_LE(in, 'f');
+    return in - 'a' + 10;
+  }
+}
+
+string AsciiEncode(const Blob &blob) {
+  static const char table[] = "0123456789abcdef";
+  string out;
+  for (Blob::const_iterator it = blob.begin(); blob.end() != it; ++it) {
+    out += table[((*it) >> 4) & 0xf];
+    out += table[*it & 0xf];
+  }
+  CHECK_EQ(blob.size() * 2, out.size());
+  return out;
+}
+
+Blob AsciiDecode(const string &str) {
+  Blob out;
+  if (str.size() % 2)
+    return out;
+  for (string::const_iterator it = str.begin(); it != str.end(); ++it) {
+    char append = DecodeChar(*it);
+    append <<= 4;
+
+    ++it;
+
+    append |= DecodeChar(*it);
+    out.push_back(append);
+  }
+  CHECK_EQ(out.size() * 2, str.size());
+  return out;
+}
 
 void* SecureMemset(void *v, int c, size_t n) {
   volatile unsigned char *p = static_cast<unsigned char *>(v);
--- src/common/chromeos/utility.h
+++ src/common/chromeos/utility.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+// Copyright (c) 2009-2010 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,8 @@
 #define CHROMEOS_UTILITY_H_
 
 #include <cstring>
+#include <string>
+#include <vector>
 
 // For use in a switch statement to return the string from a label. Like:
 // const char* CommandToName(CommandType command) {
@@ -19,6 +21,27 @@
     case label: return #label
 
 namespace chromeos {
+
+typedef std::vector<unsigned char> Blob;
+
+// Returns a string that represents the hexadecimal encoded contents of blob.
+// String will contain only the characters 0-9 and a-f.
+//
+// Parameters
+//  blob   - The bytes to encode.
+//  string - ASCII representation of the blob in hex.
+//
+std::string AsciiEncode(const Blob &blob);
+
+// Converts a string representing a sequence of bytes in hex into the actual
+// bytes.
+//
+// Parameters
+//  str - The bytes to decode.
+// Returns
+//  The decoded bytes as a Blob.
+//
+Blob AsciiDecode(const std::string &str);
 
 // Secure memset - volatile qualifier prevents a call to memset from being
 // optimized away.
